9746cf6ca7aab294954d898190e361c843d2ee68
donald nk <donaldnkouathio@gmail.com>
2021-12-30 00:28:48 +0100

    Correction des Bugs liés à la classe Offers

--- job/offers/about/index.php
+++ job/offers/about/index.php
@@ -2,8 +2,9 @@
   if (isset($_GET["id"])) {
 
     $offer = $offer->getOffer($_GET["id"]);
+    $subdomain = $subdomain->getSubdomain($offer->getId_subdomain());
 
-  	redirection($offer->getProfession()." | Alpha Omega Solutions Travail", _APP_PATH."job/offers/about/about.php", "Offres d'emplois", "Offres d'emplois", _ROOT_PATH."job/offers/about/about.css", _ROOT_PATH."job/offers/about/about.js");
+  	redirection($subdomain->getName()." | Alpha Omega Solutions Travail", _APP_PATH."job/offers/about/about.php", "Offres d'emplois", "Offres d'emplois", _ROOT_PATH."job/offers/about/about.css", _ROOT_PATH."job/offers/about/about.js");
 
   }else {
     echo "Erreur 404";
