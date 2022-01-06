0ba094813d0f6ab890e35b31efe7f68cfb0fe7be
donald nk <donaldnkouathio@gmail.com>
2022-01-07 00:04:23 +0100

    ajout de la page 'dashboard domains'

---
+++ dashboard/domains/domains.php
@@ -0,0 +1,227 @@
+<h2 class="margin-top-none" style="display: inline-block">Domaines</h2>
+
+<?php // Button for add modal ?>
+<span class="btnAdd btnAddAdmin" id="btnAddDomain">
+  <i class="material-icons vertical-align-bottom margin-right-5" style="font-size : 1.2em; margin-bottom: 1.6em">add</i>
+  <span class="">Ajouter un domaine</span>
+</span>
+
+<?php
+$domains = $domain->getDomains();
+?>
+
+<span class="stat" style="display: block">
+  <?php
+  if(count($domains) > 1){
+    echo count($domains)." domaines trouvés";
+  }else {
+    if(count($domains) > 0){
+      echo "Un domaine trouvé";
+    }else {
+      echo "Aucun domaine trouvé";
+    }
+  }
+  ?>
+</span>
+
+
+<?php // New style for domains show ?>
+<div class="suggest_container">
+  <?php
+    $i= 0;
+		foreach ($domains as $domain) {
+      if($i%2 != 0){
+        $border_color = $domain->getColor() != "" ? "border-right: 3px solid #".$domain->getColor() : "";
+      }else {
+        $border_color = $domain->getColor() != "" ? "border-left: 3px solid #".$domain->getColor() : "";
+      }
+	?>
+    <div class="suggest_block" style="<?php echo $border_color; ?>">
+      <div class="suggest_row">
+        <span class="suggest_col">Domaine No <?php echo $domain->getId(); ?></span>
+      </div>
+      <div class="suggest_row">
+        <span class="suggest_title"  style="max-width: 100%; overflow: hidden; height: 1.4em;" title="<?php echo ucfirst(htmlspecialchars_decode($domain->getName())); ?>"> <?php echo ucfirst(htmlspecialchars_decode($domain->getName())); ?></span>
+      </div>
+      <div class="suggest_row">
+        <span class="suggest_col"><i class="material-icons vertical-align-bottom margin-right-5 background-primary">today</i><?php echo get_elapsed_time($domain->getAdded_at()); ?></span>
+
+        <span class="btnEdit" id="btnEditDomain<?php echo $domain->getId(); ?>" title="Modifier cet domaine">
+          <i class="material-icons vertical-align-bottom">edit</i>
+        </span>
+        <span class="btnDelete" id="btnDeleteDomain<?php echo $domain->getId(); ?>" title="Supprimer ce domaine">
+          <i class="material-icons vertical-align-bottom">close</i>
+        </span>
+      </div>
+    </div>
+  <?php $i++;} ?>
+</div>
+
+
+<?php // Modal for add ?>
+<div class="item_deleteModal_shadow" id="addDomainModal">
+  <div class="item_deleteModal">
+    <div class="item_editPwdModal_body">
+
+      <h3 class="margin-top-none">Ajouter un domaine</h3>
+
+      <div class="item_modal_input">
+          <label for="name">Nom </label>
+          <input type="text" name="" value=""  id="name">
+      </div>
+
+      <span class="item_modal_indicator" id="add_domainIndicator"></span>
+
+    </div>
+    <div class="item_deleteModal_footer">
+      <div class="btn btn-primary" id="btnAddDomainConfirm">
+        Ajouter
+      </div>
+      <div class="btn" id="btnAddDomainClose" title="Annuler">
+        Annuler
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php foreach ($domains as $domain) { ?>
+  <input type="hidden" name="" value="<?php echo $domain->getId(); ?>"  id="id<?php echo $domain->getId(); ?>">
+
+  <?php // Modal for Edit ?>
+  <div class="item_deleteModal_shadow" id="editDomainModal<?php echo $domain->getId(); ?>">
+    <div class="item_deleteModal">
+      <div class="item_editPwdModal_body">
+
+        <h3 class="margin-top-none">Modifier le domaine</h3>
+
+        <?php $admin = $admin->getAdmin($domain->getId_admin()); ?>
+        <p>
+          Domaine No <?php echo $domain->getId(); ?>
+          <br>
+          Ajouté par <?php echo $admin->getName()." ".get_elapsed_time($domain->getAdded_at()); ?>
+        </p>
+
+        <div class="item_modal_input">
+            <label for="name<?php echo $domain->getId(); ?>">Nom </label>
+            <input type="text" name="" value="<?php echo htmlspecialchars_decode($domain->getName()); ?>"  id="name<?php echo $domain->getId(); ?>">
+        </div>
+
+        <span class="item_modal_indicator" id="edit_domainIndicator<?php echo $domain->getId(); ?>"></span>
+
+      </div>
+      <div class="item_deleteModal_footer">
+        <div class="btn btn-primary" id="btnEditDomainConfirm<?php echo $domain->getId(); ?>">
+          Modifier
+        </div>
+        <div class="btn" id="btnEditDomainClose<?php echo $domain->getId(); ?>" title="Annuler">
+          Annuler
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php // Modal for delete ?>
+  <div class="item_deleteModal_shadow" id="deleteDomainModal<?php echo $domain->getId(); ?>">
+    <div class="item_deleteModal">
+      <div class="item_deleteModal_body">
+        <i class="material-icons">warning</i>
+        <span>Voulez vous vraiment supprimer le domaine No <?php echo $domain->getId(); ?> ?</span>
+
+      </div>
+      <div class="item_deleteModal_footer">
+        <div class="btn btn-danger" id="btnDeleteDomainConfirm<?php echo $domain->getId(); ?>">
+          Supprimer
+        </div>
+        <div class="btn" id="btnDeleteDomainClose<?php echo $domain->getId(); ?>" title="Annuler">
+          Annuler
+        </div>
+      </div>
+    </div>
+  </div>
+<?php } ?>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+  	//Show or hide modal
+   function toggleModal(modal, modal_btn, modal_btn_close){
+     modal_btn.click(function(){
+       modal.fadeIn();
+     });
+     modal_btn_close.click(function(){
+       modal.fadeOut();
+     });
+    }
+
+    //For add domain in DB
+		function putInBD(btn, path, id, name){
+			btn.click(function(){
+				var id_val = id,
+    				name_val = name.val();
+
+        <?php
+          //choix de la couleur aléatoire
+          $colorTab = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+          $color= "";
+          $colorFinal= [$colorTab[random_int(0,15)], $colorTab[random_int(0,15)], $colorTab[random_int(0,15)], $colorTab[random_int(0,15)], $colorTab[random_int(0,15)], $colorTab[random_int(0,15)]];
+
+          for($i=0; $i<6; $i++){
+            $color = $color."".$colorFinal[$i];
+          }
+        ?>
+
+        var color_val = id == "" ? "<?php echo $color; ?>" : "";
+
+				$.ajax({
+					url: _ROOT_PATH+path,
+					type: "POST",
+					data:	"id="+id_val
+                +"&name="+name_val
+                +"&image="
+                +"&color="+color_val,
+					beforeSend : function(){
+						btn.html("chargement...");
+					},
+					success : function(ret){
+						window.location.reload();
+					}
+				});
+			});
+		}
+
+    //For delete offer in DB
+		function deleteInBD(btn, path, id){
+			btn.click(function(){
+				var id_val = id.val();
+
+				$.ajax({
+					url: _ROOT_PATH+path,
+					type: "POST",
+					data:	"id="+id_val,
+					beforeSend : function(){
+						btn.html("chargement...");
+					},
+					success : function(ret){
+						window.location.reload();
+					}
+				});
+			});
+		}
+
+    <?php foreach ($domains as $domain) { ?>
+      //for delete
+      toggleModal($("#deleteDomainModal<?php echo $domain->getId(); ?>"), $("#btnDeleteDomain<?php echo $domain->getId(); ?>"), $("#btnDeleteDomainClose<?php echo $domain->getId(); ?>"));
+
+      deleteInBD($("#btnDeleteDomainConfirm<?php echo $domain->getId(); ?>"), "outils/php/traitement/domain/delete-domain.php", $("#id<?php echo $domain->getId(); ?>"));
+
+      //for edit
+      toggleModal($("#editDomainModal<?php echo $domain->getId(); ?>"), $("#btnEditDomain<?php echo $domain->getId(); ?>"), $("#btnEditDomainClose<?php echo $domain->getId(); ?>"));
+
+      putInBD($("#btnEditDomainConfirm<?php echo $domain->getId(); ?>"), "outils/php/traitement/domain/edit-domain.php", $("#id<?php echo $domain->getId(); ?>").val(), $("#name<?php echo $domain->getId(); ?>"));
+    <?php } ?>
+
+    //for add
+    toggleModal($("#addDomainModal"), $("#btnAddDomain"), $("#btnAddDomainClose"));
+
+    putInBD($("#btnAddDomainConfirm"), "outils/php/traitement/domain/add-domain.php", "", $("#name"));
+  });
+</script>
