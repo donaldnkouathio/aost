fb431bf53714129b0a79000ed14786beab1fa8f3
Ego <dimcompte@gmail.com>
2022-01-13 08:23:54 +0100

    Ajout des éléments de référencement naturel SEO

--- header/head.php
+++ header/head.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.3">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.3">  
+  <meta name="author" content="Pierre Kodjio">
+  <meta http-equiv="Reply-to" content="pierrekod2@gmail.com">
 
-    <link rel="shortcut icon" href="<?php echo _ROOT_PATH; ?>img/logo.png"/>
-    <link rel="icon" sizes="144x144" href="<?php echo _ROOT_PATH; ?>img/icon.png"/>
+  <!-- META POUR REFERENCEMENT NATUREL -->
+  <meta name="Distribution" content="global">
+  <meta name="description" content="Comment trouver rapidement un emploi au canada ? AOST vous propose des offres de travail dans tous les domaines. Postulez et nous nous occupons du reste!">
+  <meta name="keywords" content="emploi,travail,offre,candidature,postuler,domain,job,work,entreprise,cv,freelance,apply,offer">
+  <meta name="robots" content="Follow">
+  <meta http-equiv="Content-language" content="fr">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
-    <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>outils/css/vars.css">
-    <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>outils/css/global.css">
-    <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>header/header.css">
-    <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>footer/footer.css">
-    <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>outils/css/emploispopulaires.css">
-    <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>outils/css/autosuggest.css">
+  <link rel="shortcut icon" href="<?php echo _ROOT_PATH; ?>img/logo.png"/>
+  <link rel="icon" sizes="144x144" href="<?php echo _ROOT_PATH; ?>img/icon.png"/>
 
-    <script type="text/javascript" src="<?php echo _ROOT_PATH; ?>outils/frameworks/jquery/jquery.js"></script>
-    <script type="text/javascript" src="<?php echo _ROOT_PATH; ?>header/header.js"></script>
-    <script type="text/javascript" src="<?php echo _ROOT_PATH; ?>outils/js/autosuggest.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Lato'>
+  <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>outils/css/vars.css">
+  <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>outils/css/global.css">
+  <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>header/header.css">
+  <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>footer/footer.css">
+  <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>outils/css/emploispopulaires.css">
+  <link rel="stylesheet" href="<?php echo _ROOT_PATH; ?>outils/css/autosuggest.css">
+
+  <script type="text/javascript" src="<?php echo _ROOT_PATH; ?>outils/frameworks/jquery/jquery.js"></script>
+  <script type="text/javascript" src="<?php echo _ROOT_PATH; ?>header/header.js"></script>
+  <script type="text/javascript" src="<?php echo _ROOT_PATH; ?>outils/js/autosuggest.js"></script>
