729779e5b04533d52c53ecee0b9359c57ad14f92
donald nk <donaldnkouathio@gmail.com>
2022-01-19 13:14:08 +0100

    Footer updated

--- footer/footer.php
+++ footer/footer.php
@@ -6,35 +6,18 @@
       </div>
 
       <div class="footer-container">
+        <div class="footer-contain">
+          <h3 class="margin-bottom-none">Siège social</h3>
+          <p class="margin-top-none">
+          <span style="margin-top: 10px;" class="material-icons vertical-align-bottom margin-right-5">location_on</span>
+            2232 Rue Noel, H4M1R9 Montréal
+          </p>
+        </div>
 
         <div class="footer-contain">
           <h3 class="margin-bottom-none">Bureau principal</h3>
 
           <p class="margin-top-none">
-            <span class="notranslate material-icons vertical-align-bottom margin-right-5">phone</span>
-            <a style="color: white" href='tel:+1 514-612-0043'>+1 514-612-0043</a> - Bureau
-            <br>
-            <span class="notranslate material-icons vertical-align-bottom margin-right-5">phone</span>
-            <a style="color: white" href='tel:+1 514-559-7850'>+1 514-559-7850</a> - Direction
-            <br>
-            <span class="notranslate material-icons vertical-align-bottom margin-right-5">phone</span>
-            <a style="color: white" href='tel:+1 514-621-7850'>+1 514-621-7850</a> - Direction
-            <br>
-            <span style="margin-top: 10px;" class="material-icons vertical-align-bottom margin-right-5">email</span>
-            <a style="color: white" href='mailto:info@alphaomegasolutionstravail.ca'> Info@alphaomegasolutionstravail.ca</a>
-            <br>
-            <span class="notranslate material-icons vertical-align-bottom margin-right-5">email</span>
-            <a style="color: white" href='mailto:rh@alphaomegasolutionstravail.ca'> RH@alphaomegasolutionstravail.ca</a>
-            <br>
-            <span class="notranslate material-icons vertical-align-bottom margin-right-5">email</span>
-            <a style="color: white" href='mailto:paie@alphaomegasolutionstravail.ca'> Paie@alphaomegasolutionstravail.ca</a>
-            <br>
-            <span class="notranslate material-icons vertical-align-bottom margin-right-5">email</span>
-            <a style="color: white" href='mailto:dr@alphaomegasolutionstravail.ca'> DR@alphaomegasolutionstravail.ca</a>
-            <br>
-            <span class="notranslate material-icons vertical-align-bottom margin-right-5">email</span>
-            <a style="color: white" href='mailto:partenaires@alphaomegasolutionstravail.ca'> Partenaires@alphaomegasolutionstravail.ca</a>
-            <br>
             <span style="margin-top: 10px;" class="material-icons vertical-align-bottom margin-right-5">location_on</span>
             880 BOULEVARD DECARIE, H4L1M3, Montréal, Saint-Laurent, Québec, Canada
             <br>
@@ -42,22 +25,44 @@
         </div>
 
         <div class="footer-contain">
-          <h3 class="margin-bottom-none">Siège social</h3>
+          <h3 class="margin-bottom-none">Suivez-nous</h3>
+          <p class="margin-top-none"><a href="https://www.facebook.com/ALPHA-OMEGA-Solutions-Travail-AOST-120693572651635/" target="_blank" style="color: white">Facebook</a></p>
+        </div>
+
+        <div class="footer-contain">
+          <h3 class="margin-bottom-none">Contactez-nous</h3>
+
           <p class="margin-top-none">
-            <span class="notranslate material-icons vertical-align-bottom margin-right-5">phone</span>
-            <a style="color: white" href='tel:+1 438-289-5095'> +1 438-289-5095</a>
+            <span style="margin-top: 10px;" class="notranslate material-icons vertical-align-bottom margin-right-5">phone</span>
+            <a style="color: white" href='tel:+1 514-612-0043'>+1 514-612-0043</a> - Bureau
             <br>
             <span class="notranslate material-icons vertical-align-bottom margin-right-5">phone</span>
-            <a style="color: white" href='tel:+1 438-938-8292'> +1 438-938-8292</a>
+            <a style="color: white" href='tel:+1 514-559-7850'>+1 514-559-7850</a> - Direction
             <br>
-            <span style="margin-top: 10px;" class="material-icons vertical-align-bottom margin-right-5">location_on</span>
-            2232 Rue Noel, H4M1R9 Montréal
+            <span class="notranslate material-icons vertical-align-bottom margin-right-5">phone</span>
+            <a style="color: white" href='tel:+1 514-621-7850'>+1 514-621-7850</a> - Direction
           </p>
         </div>
 
         <div class="footer-contain">
-          <h3 class="margin-bottom-none">Suivez-nous</h3>
-          <p class="margin-top-none"><a href="https://www.facebook.com/ALPHA-OMEGA-Solutions-Travail-AOST-120693572651635/" target="_blank" style="color: white">Facebook</a></p>
+          <h3 class="margin-bottom-none">Courriels</h3>
+
+          <p class="margin-top-none">
+            <span style="margin-top: 10px;" class="notranslate material-icons vertical-align-bottom margin-right-5">email</span>
+            <a style="color: white" class="notranslate" href='mailto:info@alphaomegasolutionstravail.ca'> Info@alphaomegasolutionstravail.ca (Informations)</a>
+            <br>
+            <span class="notranslate material-icons vertical-align-bottom margin-right-5">email</span>
+            <a style="color: white" class="notranslate" href='mailto:rh@alphaomegasolutionstravail.ca'> RH@alphaomegasolutionstravail.ca (Demande d'emploi)</a>
+            <br>
+            <span class="notranslate material-icons vertical-align-bottom margin-right-5">email</span>
+            <a style="color: white" class="notranslate" href='mailto:paie@alphaomegasolutionstravail.ca'> Paie@alphaomegasolutionstravail.ca (Paie)</a>
+            <br>
+            <span class="notranslate material-icons vertical-align-bottom margin-right-5">email</span>
+            <a style="color: white" class="notranslate" href='mailto:dr@alphaomegasolutionstravail.ca'> DR@alphaomegasolutionstravail.ca (Directeur)</a>
+            <br>
+            <span class="notranslate material-icons vertical-align-bottom margin-right-5">email</span>
+            <a style="color: white" class="notranslate" href='mailto:partenaires@alphaomegasolutionstravail.ca'> Partenaires@alphaomegasolutionstravail.ca (Partenaires)</a>
+          </p>
         </div>
 
       </div>
@@ -77,92 +82,94 @@
         </span>
       </span>
     </div>
+
+
+    <div class="alert_modal_shadow dnk">
+      <div class="alert_modal_block">
+        <div class="alert_modal_body" style="padding: 15px;">
+          <img height="80" style="border-radius: 100%;" src="<?php echo _ROOT_PATH; ?>img/authors/dnk.jpeg" alt="Donald Nkouathio picture">
+          <br>
+          <b style="margin-bottom: 15px; display: inline-block; color: black">Donald NKOUATHIO</b>
+          <br>
+          <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">phone</span>
+          <a href='tel:+237 655 203 951'> +237 655 203 951</a>
+          <br>
+          <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">email</span>
+          <a class="notranslate" href='mailto:donaldnkouathio@gmail.com'> Donaldnkouathio@gmail.com</a>
+        </div>
+
+        <div class="alert_modal_footer">
+          <button type="button" class="btn alert_modal_close" name="button">Fermer</button>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="alert_modal_shadow dim">
+      <div class="alert_modal_block">
+        <div class="alert_modal_body" style="padding: 15px;">
+          <img height="80" style="border-radius: 100%;" src="<?php echo _ROOT_PATH; ?>img/authors/dim.jpeg" alt="Dimitry Beyene picture">
+          <br>
+          <b style="margin-bottom: 15px; display: inline-block; color: black">Dimitri BEYENE</b>
+          <br>
+          <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">phone</span>
+          <a href='tel:+237 692 503 797'> +237 692 503 797</a>
+          <br>
+          <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">email</span>
+          <a class="notranslate" href='mailto:dimcompte@gmail.com'> Dimcompte@gmail.com</a>
+        </div>
+
+        <div class="alert_modal_footer">
+          <button type="button" class="btn alert_modal_close" name="button">Fermer</button>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="alert_modal_shadow dil">
+      <div class="alert_modal_block">
+        <div class="alert_modal_body" style="padding: 15px;">
+          <img height="80" style="border-radius: 100%;" src="<?php echo _ROOT_PATH; ?>img/authors/dil.jpeg" alt="Dilane Ohno picture">
+          <br>
+          <b style="margin-bottom: 15px; display: inline-block; color: black">Dilane OHNO</b>
+          <br>
+          <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">phone</span>
+          <a href='tel:+237 697 494 078'> +237 697 494 078</a>
+          <br>
+          <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">email</span>
+          <a class="notranslate" href='mailto:ohnodilane@gmail.com'> Ohnodilane@gmail.com</a>
+        </div>
+
+        <div class="alert_modal_footer">
+          <button type="button" class="btn alert_modal_close" name="button">Fermer</button>
+        </div>
+      </div>
+    </div>
+
+    <script type="text/javascript" src="<?php echo _ROOT_PATH; ?>outils/js/global.js"></script>
+    <script>
+      $(document).ready(function(){
+        //Show or hide modal
+        function toggleModal(modal, modal_btn, modal_btn_close){
+         $(".alert_modal_block").click(function(ev){
+           ev.stopPropagation();
+         });
+         modal_btn.click(function(){
+           modal.fadeIn();
+         });
+         modal_btn_close.click(function(){
+           modal.fadeOut();
+         });
+       }
+
+       toggleModal($(".dnk"), $(".show_about_author_dnk"), $(".alert_modal_close, .alert_modal_shadow"));
+       toggleModal($(".dim"), $(".show_about_author_dim"), $(".alert_modal_close, .alert_modal_shadow"));
+       toggleModal($(".dil"), $(".show_about_author_dil"), $(".alert_modal_close, .alert_modal_shadow"));
+     });
+    </script>
+
+
   </footer>
 
 </body>
 </html>
-
-
-<div class="alert_modal_shadow dnk">
-  <div class="alert_modal_block">
-    <div class="alert_modal_body" style="padding: 15px;">
-      <img height="80" style="border-radius: 100%;" src="<?php echo _ROOT_PATH; ?>img/authors/dnk.jpeg" alt="Donald Nkouathio picture">
-      <br>
-      <b style="margin-bottom: 15px; display: inline-block">Donald NKOUATHIO</b>
-      <br>
-      <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">phone</span>
-      <a href='tel:+237 655 203 951'> +237 655 203 951</a>
-      <br>
-      <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">email</span>
-      <a href='mailto:donaldnkouathio@gmail.com'> Donaldnkouathio@gmail.com</a>
-    </div>
-
-    <div class="alert_modal_footer">
-      <button type="button" class="btn alert_modal_close" name="button">Fermer</button>
-    </div>
-  </div>
-</div>
-
-
-<div class="alert_modal_shadow dim">
-  <div class="alert_modal_block">
-    <div class="alert_modal_body" style="padding: 15px;">
-      <img height="80" style="border-radius: 100%;" src="<?php echo _ROOT_PATH; ?>img/authors/dim.jpeg" alt="Dimitry Beyene picture">
-      <br>
-      <b style="margin-bottom: 15px; display: inline-block">Dimitri BEYENE</b>
-      <br>
-      <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">phone</span>
-      <a href='tel:+237 692 503 797'> +237 692 503 797</a>
-      <br>
-      <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">email</span>
-      <a href='mailto:dimcompte@gmail.com'> Dimcompte@gmail.com</a>
-    </div>
-
-    <div class="alert_modal_footer">
-      <button type="button" class="btn alert_modal_close" name="button">Fermer</button>
-    </div>
-  </div>
-</div>
-
-
-<div class="alert_modal_shadow dil">
-  <div class="alert_modal_block">
-    <div class="alert_modal_body" style="padding: 15px;">
-      <img height="80" style="border-radius: 100%;" src="<?php echo _ROOT_PATH; ?>img/authors/dil.jpeg" alt="Dilane Ohno picture">
-      <br>
-      <b style="margin-bottom: 15px; display: inline-block">Dilane OHNO</b>
-      <br>
-      <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">phone</span>
-      <a href='tel:+237 697 494 078'> +237 697 494 078</a>
-      <br>
-      <span class="notranslate material-icons vertical-align-bottom margin-right-5 background-primary">email</span>
-      <a href='mailto:ohnodilane@gmail.com'> Ohnodilane@gmail.com</a>
-    </div>
-
-    <div class="alert_modal_footer">
-      <button type="button" class="btn alert_modal_close" name="button">Fermer</button>
-    </div>
-  </div>
-</div>
-
-<script type="text/javascript" src="<?php echo _ROOT_PATH; ?>outils/js/global.js"></script>
-<script>
-  $(document).ready(function(){
-    //Show or hide modal
-    function toggleModal(modal, modal_btn, modal_btn_close){
-     $(".alert_modal_block").click(function(ev){
-       ev.stopPropagation();
-     });
-     modal_btn.click(function(){
-       modal.fadeIn();
-     });
-     modal_btn_close.click(function(){
-       modal.fadeOut();
-     });
-   }
-
-   toggleModal($(".dnk"), $(".show_about_author_dnk"), $(".alert_modal_close, .alert_modal_shadow"));
-   toggleModal($(".dim"), $(".show_about_author_dim"), $(".alert_modal_close, .alert_modal_shadow"));
-   toggleModal($(".dil"), $(".show_about_author_dil"), $(".alert_modal_close, .alert_modal_shadow"));
- });
-</script>
