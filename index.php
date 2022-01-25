e080406351dc1ec615910d0bfc0b7c7bcc6dd5fd
donald nk <donaldnkouathio@gmail.com>
2022-01-25 16:49:30 +0100

    SEO updated

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
-<?php
-    header("location: home/");
+<?php include("outils/php/redirection.php");
+
+  redirection("Alpha Omega Solutions Travail", _APP_PATH."home/home.php", "home", "", _ROOT_PATH."home/home.css", _ROOT_PATH."home/home.js");
+
 ?>
